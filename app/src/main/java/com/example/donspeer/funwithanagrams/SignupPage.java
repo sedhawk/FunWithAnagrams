8282798a98a2a7b6464bfe673c993312c3068b17
sedhawk <speerdw@gmail.com>
2017-10-16 17:40:49 -0700

    fixed signup button

--- app/src/main/java/com/example/donspeer/funwithanagrams/SignupPage.java
+++ app/src/main/java/com/example/donspeer/funwithanagrams/SignupPage.java
@@ -1,13 +1,29 @@
 package com.example.donspeer.funwithanagrams;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupPage extends AppCompatActivity {
+
+    Button create_account_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_page);
+
+        create_account_button = findViewById(R.id.signup_button);
+
+        create_account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent);
+            }
+        })  ;
     }
 }
